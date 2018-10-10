142119ff51bb01649dbf28796dbf7bb0b047edfb
aott01 <aott01@gmail.com>
2018-10-10 11:49:42 -0700

    portsnap.sh moved from CVS
    runs portsnap fetch/extract/update

---
+++ portsnap.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# $Id: portsnap.sh,v 1.1 2018/10/10 18:47:54 andreas Exp $
+
+/usr/sbin/portsnap fetch 2>&1 | /usr/bin/tee -a /var/log/portsnap.log 2>&1
+#only one time
+#/usr/sbin/portsnap extract 2>&1 | /usr/bin/tee -a /var/log/portsnap.log 2>&1
+#after that update only
+/usr/sbin/portsnap update 2>&1 | /usr/bin/tee -a /var/log/portsnap.log 2>&1
+
+/usr/bin/logger $0 completed
+exit 0
+###EOF script
+#
+# $Log: portsnap.sh,v $
+# Revision 1.1  2018/10/10 18:47:54  andreas
+# runs portsnap fetch/extract/update
+#
+# first time under CVS
+#
