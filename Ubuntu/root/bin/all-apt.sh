97d7b9c5691f49790d7fb1f694847523dcb98d60
Andreas Ott <aott01@gmail.com>
2024-07-11 12:37:17 -0700

    Update all-apt.sh
    list upgradable, show last apt logs and check for needsrestart

--- Ubuntu/root/bin/all-apt.sh
+++ Ubuntu/root/bin/all-apt.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-set -e
+set -ev
 
 /usr/bin/apt-get update
+/usr/bin/apt list --upgradable
 /usr/bin/apt-get -y dist-upgrade
 /usr/bin/apt-get -y autoremove
 /usr/bin/apt-get -y autoclean
+/usr/bin/tac /var/log/apt/history.log | /usr/bin/awk '!flag; /Start-Date: /{flag = 1};' | /usr/bin/tac
+
+/usr/bin/grep VERSION= /etc/os-release 
+/usr/bin/ls -lart /var/run/needrestart/
