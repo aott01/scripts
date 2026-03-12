70ab1b3e7723a24d6f66136d1fc414b7c63df542
Andreas Ott <aott01@gmail.com>
2026-03-12 13:12:39 -0700

    Update portsnap.sh
    portsnap has been retired

--- FreeBSD/root/bin/portsnap.sh
+++ FreeBSD/root/bin/portsnap.sh
@@ -2,6 +2,9 @@
 
 # $Id: portsnap.sh,v 1.1 2018/10/10 18:47:54 andreas Exp $
 
+echo portsnap has been retired - use gitup ports
+exit 0
+
 /usr/sbin/portsnap fetch 2>&1 | /usr/bin/tee -a /var/log/portsnap.log 2>&1
 #only one time
 #/usr/sbin/portsnap extract 2>&1 | /usr/bin/tee -a /var/log/portsnap.log 2>&1
